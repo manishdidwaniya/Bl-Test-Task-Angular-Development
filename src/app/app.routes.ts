88150ade590c4aa858c0493e62f606baa4faa78a
manish <manishdidwaniyaa@gmail.com>
2024-10-02 16:51:23 +0530

    created sign-in component and added route for it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import {Routes} from '@angular/router';
 
 const signUpRoute: string = 'sign-up';
+const signInRoute: string = 'sign-in';
 
 export const routes: Routes = [
-  {path: signUpRoute, loadComponent: () => import('./features/session/sign-up/sign-up.component').then(m => m.SignUpComponent)},
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: signInRoute,
+    pathMatch: 'full'
+  },
+  {
+    path: signUpRoute, loadComponent: () => import('./features/session/sign-up/sign-up.component').then(m => m.SignUpComponent)
+  },
+  {
+    path: signInRoute, loadComponent: ()=> import('./features/session/sign-in/sign-in.component').then(m => m.SignInComponent)
   }
 ];
